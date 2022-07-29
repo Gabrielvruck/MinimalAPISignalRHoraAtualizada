e2d52dafb83f2bd376b56e2e3b86a7b11fbac1d7
Gabriel Vruck <gabrielvruck@gmail.com>
2022-07-29 13:07:38 -0300

    criacao servidor signalr e signalr client para atualizar a hora

---
+++ MiniSignalClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+const string url = "https://localhost:7201/chat";
+
+//criacao da conexao com servidor signalr
+await using var connection = new HubConnectionBuilder().WithUrl(url).Build();
+
+//conectar ao servidor
+await connection.StartAsync();
+
+await foreach(var date in connection.StreamAsync<DateTime>("Streaming"))
+{
+    Console.WriteLine(date);
+}
---
+++ MiniSignalServer/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.SignalR;
+
+var builder = WebApplication.CreateBuilder(args);
+//adicionar serviço signalR
+builder.Services.AddSignalR();
+var app = builder.Build();
+
+app.MapHub<MyHub>("/chat");
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
+class MyHub : Hub
+{
+    public async IAsyncEnumerable<DateTime> Streaming(CancellationToken cancellationToken)
+    {
+        while (true)
+        {
+            yield return DateTime.UtcNow;
+            await Task.Delay(1000, cancellationToken);
+        }
+    }
+}
